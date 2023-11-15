fe80f1773d9f7c7a989735b542b9dd97a4bba49a
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-11-14 20:35:49 -0500

    Add code to show food and scoreboard

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
+from scoreboard import  ScoreBoard
 import time
 
 screen = Screen()
@@ -9,6 +11,9 @@ screen.title("My snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
 game_is_on = True
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,5 +25,13 @@ while(game_is_on):
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
