20871e56d7da25fea3d1a9bea9e0324f00e0e473
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-11-27 08:33:42 -0500

    Adding code to move ball with paddles

--- PongGame/main.py
+++ PongGame/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.tracer(0)
@@ -9,7 +11,7 @@ screen.title("Pong")
 
 right_paddle = Paddle((350,0))
 left_paddle = Paddle((-350,0))
-
+ball = Ball()
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
 screen.onkey(right_paddle.go_down, "Down")
@@ -19,5 +21,24 @@ screen.onkey(left_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collision with top and bottom wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+       ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # when Right paddle missed ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # when Right paddle missed ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
