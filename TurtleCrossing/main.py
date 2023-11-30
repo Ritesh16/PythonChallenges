a3ca67be6cb2cb429036bc5fde2264019b15a9ad
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-11-29 22:34:15 -0500

    Adding code for turtle crossing game

---
+++ TurtleCrossing/main.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+import time
+
+from player import Player
+from car_manager import CarManager
+
+screen = Screen()
+screen.tracer(0)
+screen.setup(width=600, height= 600)
+screen.bgcolor("white")
+
+player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_car()
+
+    #Detect collision
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
---
+++ TurtleCrossing/player.py
@@ -0,0 +1,14 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("turtle")
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
