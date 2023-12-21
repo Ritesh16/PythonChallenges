90f84c42f4fe0a054fab094c5aa3ffd4b97b5972
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-12-21 10:06:16 -0500

    Adding code to complete guess game

--- USStates/main.py
+++ USStates/main.py
@@ -8,13 +8,37 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states guessed",
+                                    prompt="What's another state name ?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+turtle.mainloop()
+
+# How to get coordinates
 #def get_mouse_click_coor(x, y):
  #   print(x,y)
 
 #turtle.onscreenclick(get_mouse_click_coor)
-
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state name ?")
-print(answer_state)
-turtle.mainloop()
-
