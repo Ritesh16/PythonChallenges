484728bbb2c7d6b3c6f17b53943729d2532046cf
Ritesh16 <ritesh.sharma.cse@gmail.com>
2024-01-29 12:44:57 -0500

    Adding validation on save

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,9 +1,18 @@
 from tkinter import *
-
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="OOPS", message= "Please add valid website or password")
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -11,7 +20,7 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 canvas = Canvas(height= 200, width= 200)
 logo = PhotoImage(file="logo.png")
-canvas.create_image(100, 112, image=logo)
+canvas.create_image(100, 100, image=logo)
 
 canvas.grid(row = 0, column=1)
 
@@ -29,13 +38,14 @@ website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
 email_entry = Entry(width= 35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "ritesh@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 #Button
 generate_button = Button(text="Generate Button")
 generate_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
