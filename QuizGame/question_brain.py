d9e182077e621d3e46911dacc793155af206e89c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2024-04-12 14:01:08 -0400

    Completing the quiz app

--- QuizGame/question_brain.py
+++ QuizGame/question_brain.py
@@ -16,14 +16,12 @@ class QuestionBrain:
         #user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
         #self.check_answer(user_answer, current_question.answer)
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            print("You got it right!")
             self.score += 1
+            return True
         else:
-            print("That's wrong")
+            return False
 
-        print(f"Correct answer was: {correct_answer}")
-        print(f"Your current score is: {self.score}")
-        print("\n")
 
